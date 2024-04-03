41256a1d49740d1e0c07d0efd59a1fa058a5ebd6
vitorlopes079 <vitorlopes079@gmail.com>
2024-04-03 19:33:33 +0800

    HeroHomePage component created to make the homepage cleaner

--- src/app/(pages)/page.js
+++ src/app/(pages)/page.js
@@ -1,8 +1,6 @@
 import Image from "next/image";
 import styles from "../css/homepage.module.css";
-import heroscreeshot from "../images/heroscreeshot.png";
 import arrowUp from "../images/arrow-up.png";
-import arrowDown from "../images/arrow-down.svg";
 import avatar from "../images/avatar.png";
 import foldIcon from "../images/foldIcon.png";
 import aiIcon from "../images/aiIcon.png";
@@ -27,64 +25,15 @@ import Stack from "../_components/Stack";
 import chatbotChatImage from "../images/chatbotConv.svg";
 import RoundedWidget from "../_components/RoundedWidget";
 import MarketingCard from "../_components/MarketingCard";
-import imageCover from "../images/imageCover.png";
 import Testimonies from "../_components/Testimonies";
+import HeroHomePage from "../_components/HeroHomePage";
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Elipse numberOfEllipses={7} />
-      <section className={styles.hero}>
-        <RoundedWidget text={"Fast & Efficient with AI"} star={true} />
-
-        <div className={styles.heroHeadingContainer}>
-          <h1 className="centralizedHeading">
-            Help customer and employees instantly with a personalized
-            <span className={styles.firstLine}>&nbsp;AI-driven chatbot</span>
-          </h1>
-          <p className="centralizedText">
-            Your customers and employees deserve more than just a basic chatbot.
-            Elevate their experience with comprehensive customer service
-            automation, human resource inquiry automation, and an AI-powered
-            e-learning guide that efficiently resolves inquiries while
-            delivering an unparalleled user experience. Don&apos;t settle for
-            mediocrity; choose innovation and excellence and invest in the
-            future for your business.
-          </p>
-        </div>
-        <div>
-          <button className="buttonYellow buttonMargin">
-            Get Started
-            <Image src={arrowUp} width={32} height={32} alt="arrow up" />
-          </button>
-          <button className="buttonGrey buttonMargin">
-            Learn More
-            <Image src={arrowDown} width={32} height={32} alt="arrow down" />
-          </button>
-        </div>
-        <div className={styles.heroImageContainer}>
-          <div className={styles.heroscreeshotContainer}>
-            <Image
-              src={heroscreeshot}
-              width={869}
-              height={621}
-              alt="Dashboard Screenshot"
-              className={styles.heroImage}
-            />
-            <div className={`gradientCover ${styles.gradientCoverHero}`}></div>
-          </div>
-
-          <div className={styles.heroImageCoverContainer}>
-            <Image
-              src={imageCover}
-              width={273}
-              height={345}
-              alt="Ai-chat screenchot"
-            />
-          </div>
-        </div>
-      </section>
-
+      <HeroHomePage />
+      
       <section className={styles.avatarSection}>
         <div className={styles.avatarTopDiv}>
           <div className={styles.avatarSectionTextContainer}>
